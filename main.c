96f3e3dcf44bd96a5c786046a1dde449fba6125d
Dean Ross <dean.h.ross@uon.edu.au>
2019-04-04 05:29:54 +0000

    added encryption algorithm

--- main.c
+++ main.c
@@ -13,12 +13,14 @@ int main() {
 
         if (text[i]<65 || text[i]>91) { //keeps value the same if it is not a letter
             encrypt[i] = text[i];
-            i=i+1;
         }
         else {
     encrypt[i]= text[i] + key; 
-}
-        
+             }
+        if (encrypt[i]>91) {
+            encrypt[i]=encrypt[i]-26;
+        }
+
  i=i+1;
 }
 printf("%s", encrypt);
