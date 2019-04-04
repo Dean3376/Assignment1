e94401b18848f58ec5b80815c0c06a27770d4926
Dean Ross <dean.h.ross@uon.edu.au>
2019-04-04 05:54:07 +0000

    edited algorithm for edge cases

--- main.c
+++ main.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    char text[] = "this is a test message";
-    int key=24, n = 0,i=0;
+    char text[] = "zzzzzzzzz";
+    int key=4, n = 0,i=0;
     n = sizeof(text); // gets the amount of characters in original message
     char encrypt[n]; // creates array to store the encryped message
     printf("%d\n",n);
     while (i<n) {
-        if (text[i]<123 && text[i]>96) { //changes lowercase into uppercase
+        if ((text[i]<123) && (text[i]>96)) { //changes lowercase into uppercase
             text[i]= text[i]-32;
         }
 
-        if (text[i]<65 || text[i]>91) { //keeps value the same if it is not a letter
-            encrypt[i] = text[i];
+        if ((text[i]>64) && (text[i]<91)) { //keeps value the same if it is not a letter
+            encrypt[i]= text[i] + key; 
+            if (encrypt[i]>91) {
+            encrypt[i]=encrypt[i]-26; // makes
+            }
+
         }
         else {
-    encrypt[i]= text[i] + key; 
+    encrypt[i] = text[i];
              }
-        if (encrypt[i]>91) {
-            encrypt[i]=encrypt[i]-26;
-        }
-
+        
  i=i+1;
 }
-printf("this is the encrypted string: %s", encrypt);
+printf("this is the encrypted string: %s", encrypt); //prints the encrypted message
 
     return 0;
 }
